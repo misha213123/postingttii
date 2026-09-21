17547f2488679f9d8b1619348cb396f0b8410d10
misha213123 <misha0228050@gmail.com>
2026-09-21 12:07:36 +0200

    Export browser profiles router

--- app/account_creator/api/__init__.py
+++ app/account_creator/api/__init__.py
@@ -1,5 +1,6 @@
 from .accounts import router as accounts_router
 from .aliases import router as aliases_router
 from .profile import router as profile_router
+from .browser_profiles import router as browser_profiles_router
 
-__all__ = ["accounts_router", "aliases_router", "profile_router"]
+__all__ = ["accounts_router", "aliases_router", "profile_router", "browser_profiles_router"]
